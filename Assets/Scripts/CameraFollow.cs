9fa75c6826fca0d3fac34851aeddbe470a1740bf
AlexKonev <alexkonev777@gmail.com>
2023-03-03 10:58:57 +0300

    Camera Follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CameraFollow : MonoBehaviour {
+    public Transform target; // —сылка на трансформ игрока, за которым следит камера
+    public float distance = 10f; // –ассто€ние камеры до игрока по оси Z
+    public float height = 5f; // ¬ысота камеры над игроком
+    public float smoothSpeed = 0.125f; // —корость перемещени€ камеры
+
+    private Vector3 offset; // ¬ектор смещени€ между камерой и игроком
+
+    private void Start() {
+        offset = transform.position - target.position; // ¬ычисл€ем вектор смещени€ между камерой и игроком при инициализации
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void LateUpdate() {
+        // ¬ычисл€ем позицию камеры на основе позиции игрока и вектора смещени€
+        Vector3 desiredPosition = new Vector3(target.position.x, target.position.x, target.position.z+5f) + offset + Vector3.up * height - Vector3.forward * distance;
+
+        // ѕлавно перемещаем камеру к желаемой позиции
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        // Ќаправл€ем камеру на игрока
+        //transform.LookAt(target.position + Vector3.up * height - Vector3.forward * 2f);
     }
 }
